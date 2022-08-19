932441ddb7c1afad543d145bdf9e0400042a5439
djflashpoint <djflashpoint@users.noreply.github.com>
2022-08-18 22:45:55 -0700

    Add files via upload

---
+++ String2Hex.cpp
@@ -0,0 +1,17 @@
+﻿#include "String2Hex.h"
+using namespace std;
+int main() {
+	while (true)
+	{
+		std::cout << "Enter a string you want to convert to Hexadecimal: ";
+		string s1;
+		getline(std::cin, s1);
+		std::cout << "\nString: " << s1 << std::endl;
+		std::cout << "\nHexadecimal: ";
+		for (int i = 0; i < s1.size(); i++) {
+			int character = int(s1[i]);
+			std::cout << hex << character;
+		}
+		std::cout << '\n' << std::endl;
+	}
+}
